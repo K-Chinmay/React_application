2eade4d57801b935b497c66a95a9e73a063d3428
unknown <chinmay99v@gmail.com>
2022-02-24 01:20:54 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Main from "./components/MainComponent";
 import { render } from "@testing-library/react";
 import { Component } from "react/cjs/react.production.min";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
