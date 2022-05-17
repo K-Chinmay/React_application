ecf6b43ccda743ffcedbfa4f8c6ad1fa591bd97f
unknown <chinmay99v@gmail.com>
2022-05-18 01:26:54 +0530

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,17 @@
-import { createStore } from "redux"; //allows us to create store
-import { Reducer, initialState } from "./reducer";
-
+import { createStore, combineReducers } from "redux"; //allows us to create store
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
 export const ConfigureStore = () => {
-  const store = createStore(Reducer, initialState);
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+    })
+  );
 
   return store;
 };
